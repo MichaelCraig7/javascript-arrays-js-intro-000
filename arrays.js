89106aa16522d02a09dca1a28a7a6915ff7d0e9e
Michael Craig <m_craig@baylor.edu>
2018-04-10 16:15:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,10 +29,14 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
 }
 
 function removeElementFromBeginningOfArray (array) {
-  return array.slice(0);
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray (array) {
   array.pop();
   return array;
+}
+
+function removeElementFromEndOfArray (array) {
+  return array.slice(0, array.length - 1);
 }
